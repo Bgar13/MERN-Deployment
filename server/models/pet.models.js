032a77ebdf572dddf2245d40d60ba09f0b7985b8
Brenda De la torre <brendadelatorre@Brendas-MacBook-Pro.local>
2021-04-28 21:02:53 -0700

    fixed pet model

--- server/models/pet.models.js
+++ server/models/pet.models.js
@@ -4,17 +4,17 @@ const PetSchema = new mongoose.Schema({
     petName:{
         type: String,
         required:[true, "Pet Name is required"],
-        minLenght:[2, "Pet name should be at least 3 characters long"],
+        minlenght:[3, "Pet name should be at least 3 characters long"],
     },
     typeOfPet:{
         type: String,
         required:[true, "Pet Type is required"],
-        minLenght:[3, "Pet type should be at least 3 characters long"],
+        minlenght:[3, "Pet type should be at least 3 characters long"],
     },
     description:{
         type: String,
         required:[true, "Please describe your pet"],
-        minLenght:[3, "Pet description should be at least 3 characters long"],
+        minlenght:[3, "Pet description should be at least 3 characters long"],
     },
     skillOne:{
         type: String,
